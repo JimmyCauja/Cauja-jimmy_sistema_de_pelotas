7332fabe668aa950d5df5e7db2b41ee1440cf6c9
JimmyCauja <jimmycauja@gmail.com>
2023-10-05 16:53:18 -0500

    Se creo una clase para las pelotas y un archivo llamado pelota

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
+let miPelota;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  miPelota = new Pelota();
 }
 
 function draw() {
+  background(200);
   circle(mouseX, mouseY, 20);
+  miPelota.update(300);
+  miPelota.display();
 }
